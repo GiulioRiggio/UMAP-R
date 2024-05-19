56e0e8ba206248a1fb53ed9c2e8060b0ec5490f8
Giulio Riggio <giulio.riggio.73@gmail.com>
2024-05-19 12:04:50 +0200

    Creazione matrice di dati simulati con Splatter e proiezione con UMAP

---
+++ 2024-05-19 test1.R
@@ -0,0 +1,28 @@
+# citation 
+
+library(splatter)
+library(scater)
+
+params <- newSplatParams()
+params
+
+# Set multiple parameters at once (using a list)
+params <- setParams(params, update = list(nGenes = 2000, batchCells = 5000))
+
+#nGenes = 2000, batchCells = 5000 impiega un minuto 
+
+# Creo l'oggetto sim
+sim <- splatSimulate(params)
+
+matrice_generata <- counts(sim)
+
+# Creo la matrice trasposta
+matrice_UMAP <- t(matrice_generata)
+
+fit2 <- densvis::umap(matrice_UMAP[, 1:1000], densmap = FALSE)  
+ggplot() +
+  aes(fit2[, 1], fit2[, 2]) +
+  geom_point(pch = 19) +
+  ggtitle("UMAP con Splatter") +
+  labs(x = "UMAP1", y = "UMAP2")
+
