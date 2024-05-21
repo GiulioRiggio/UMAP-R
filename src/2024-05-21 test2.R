4fc9cf2763da661d928d9e79cfd1facfc6b4630b
Giulio Riggio <giulio.riggio.73@gmail.com>
2024-05-21 11:27:11 +0200

    add file

--- src/2024-05-21 test2.R
+++ src/2024-05-21 test2.R
@@ -1,4 +1,5 @@
 # test folder
 
 p_load(readr)
+depression_heart_failure <- read_csv("C:\\Users\\giuli\\Documents\\Stage - tesi\\01 UMAP-R stage-tesi\\data\\EHRs_datasets_for_clustering\\journal.pone.0158570_S2File_depression_heart_failure.csv")
 
