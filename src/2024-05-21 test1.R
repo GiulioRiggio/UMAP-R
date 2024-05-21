bfeab7ae1d93b16ec936c6399705ae493eb218f7
Giulio Riggio <giulio.riggio.73@gmail.com>
2024-05-21 11:46:40 +0200

    Chiariti i nomi in environment

--- src/2024-05-21 test1.R
+++ src/2024-05-21 test1.R
@@ -2,18 +2,17 @@
 # library(pacman)
 
 p_load(readr)
-data2 <- read_csv("C:\\Users\\giuli\\Documents\\Stage - tesi\\01 UMAP-R stage-tesi\\data\\EHRs_datasets_for_clustering\\journal.pone.0148699_S1_Text_Sepsis_SIRS_EDITED.csv")
-
+load_Text_Sepsis_SIRS_EDITED <- read_csv("C:\\Users\\giuli\\Documents\\Stage - tesi\\01 UMAP-R stage-tesi\\data\\EHRs_datasets_for_clustering\\journal.pone.0148699_S1_Text_Sepsis_SIRS_EDITED.csv")
 
 # Segna l'inizio della computazione
 start_time <- Sys.time()
 
-fit2 <- densvis::umap(data2[,], densmap = FALSE, n_neighbors = 100L, min_dist = 0.1)  
+Text_Sepsis_SIRS_EDITED <- densvis::umap(load_Text_Sepsis_SIRS_EDITED[,], densmap = FALSE, n_neighbors = 30L, min_dist = 0.2)  
 ggplot() +
-  aes(fit2[, 1], fit2[, 2], color = 1) +
+  aes(Text_Sepsis_SIRS_EDITED[, 1], Text_Sepsis_SIRS_EDITED[, 2], color = 1) +
   geom_point(pch = 19) +
   #scale_colour_discrete(name = "Gruppi") +
-  ggtitle("UMAP mammoth") +
+  ggtitle("UMAP Text_Sepsis_SIRS_EDITED") +
   labs(x = "UMAP1", y = "UMAP2")+
   theme(legend.position = "none")
 
--- src/2024-05-21 test2.R
+++ src/2024-05-21 test2.R
@@ -1,15 +1,14 @@
 # File del prof depression_heart_failure 
 
 p_load(readr)
-depression_heart_failure <- read_csv("C:\\Users\\giuli\\Documents\\Stage - tesi\\01 UMAP-R stage-tesi\\data\\EHRs_datasets_for_clustering\\journal.pone.0158570_S2File_depression_heart_failure.csv")
-
+load_depression_heart_failure <- read_csv("C:\\Users\\giuli\\Documents\\Stage - tesi\\01 UMAP-R stage-tesi\\data\\EHRs_datasets_for_clustering\\journal.pone.0158570_S2File_depression_heart_failure.csv")
 
 # Segna l'inizio della computazione
 start_time <- Sys.time()
 
-fit2 <- densvis::umap(depression_heart_failure[,2:15], densmap = FALSE, n_neighbors = 30L, min_dist = 0.15)  
+depression_heart_failure <- densvis::umap(load_depression_heart_failure[,2:15], densmap = FALSE, n_neighbors = 30L, min_dist = 0.15)  
 ggplot() +
-  aes(fit2[, 1], fit2[, 2], color = 1) +
+  aes(depression_heart_failure[, 1], depression_heart_failure[, 2], color = 1) +
   geom_point(pch = 19) +
   #scale_colour_discrete(name = "Gruppi") +
   ggtitle("UMAP depression_heart_failure") +
