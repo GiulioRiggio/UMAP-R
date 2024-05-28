967b63e430d8cc6e35e40faec16f98b1a442eb9b
Giulio Riggio <giulio.riggio.73@gmail.com>
2024-05-28 15:42:02 +0200

    Modificato l'uso della funzione ggplot() in modo da usare dataframe e successivamente geom_point(). Modificare tutti gli altri script allo stesso modo.

--- src/2024-05-21 test1.R
+++ src/2024-05-21 test1.R
@@ -1,20 +1,22 @@
 # File del prof Text_Sepsis_SIRS_EDITED
-# library(pacman)
 
+library(pacman)
 p_load(readr)
+library(ggplot2)
 load_Text_Sepsis_SIRS_EDITED <- read_csv("C:\\Users\\giuli\\Documents\\Stage - tesi\\01 UMAP-R stage-tesi\\data\\EHRs_datasets_for_clustering\\journal.pone.0148699_S1_Text_Sepsis_SIRS_EDITED.csv")
 
 # Segna l'inizio della computazione
 start_time <- Sys.time()
 
-Text_Sepsis_SIRS_EDITED <- densvis::umap(load_Text_Sepsis_SIRS_EDITED[,], densmap = FALSE, n_neighbors = 30L, min_dist = 0.2)  
-ggplot() +
-  aes(Text_Sepsis_SIRS_EDITED[, 1], Text_Sepsis_SIRS_EDITED[, 2], color = 1) +
+Text_Sepsis_SIRS_EDITED <- densvis::umap(load_Text_Sepsis_SIRS_EDITED[,], densmap = FALSE, n_neighbors = 5L, min_dist = 0.2)  
+# Convertiamo la matrice in un data frame
+df_Text_Sepsis_SIRS_EDITED <- as.data.frame(Text_Sepsis_SIRS_EDITED)
+mio_grafico <- ggplot(df_Text_Sepsis_SIRS_EDITED, aes(df_Text_Sepsis_SIRS_EDITED[, 1], df_Text_Sepsis_SIRS_EDITED[, 2], color = 1)) +
   geom_point(pch = 19) +
   #scale_colour_discrete(name = "Gruppi") +
   ggtitle("UMAP Text_Sepsis_SIRS_EDITED") +
   labs(x = "UMAP1", y = "UMAP2")+
-  theme(legend.position = "none")
+  theme(legend.position = "none") 
 
 # Segna la fine della computazione
 end_time <- Sys.time()
