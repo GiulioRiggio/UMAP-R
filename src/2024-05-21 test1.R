3c10c4158526f27dd21665b7757b5c487068c179
Giulio Riggio <giulio.riggio.73@gmail.com>
2024-05-29 17:24:47 +0200

    Rinomino le colonne del dataframe della proiezione UMAP. Aggiunta la merge dei due dataframe. # Unisci i due data frame Ho tutto in un unico dataframe.

--- src/2024-05-21 test1.R
+++ src/2024-05-21 test1.R
@@ -12,8 +12,12 @@ start_time <- Sys.time()
 num_vicini = 5L
 distanza_min = 0.001
 Text_Sepsis_SIRS_EDITED <- densvis::umap(load_Text_Sepsis_SIRS_EDITED[,], densmap = FALSE, n_neighbors = num_vicini, min_dist = distanza_min)  
+# Rinomina le colonne
+colnames(Text_Sepsis_SIRS_EDITED) <- c("UMAP1", "UMAP2")
+
 # Convertiamo la matrice in un data frame
 df_Text_Sepsis_SIRS_EDITED <- as.data.frame(Text_Sepsis_SIRS_EDITED)
+
 mio_grafico <- ggplot(df_Text_Sepsis_SIRS_EDITED, aes(df_Text_Sepsis_SIRS_EDITED[, 1], df_Text_Sepsis_SIRS_EDITED[, 2], color = 1)) +
   geom_point(pch = 19) +
   #scale_colour_discrete(name = "Gruppi") +
@@ -29,6 +33,11 @@ end_time <- Sys.time()
 # Calcola la durata totale
 duration <- end_time - start_time
 
+# Unisci i due data frame
+merged_df <- cbind(load_Text_Sepsis_SIRS_EDITED, Text_Sepsis_SIRS_EDITED)
+
+
+
 # Stampa la durata
 print(paste("Tempo totale:", duration))
 
@@ -41,3 +50,12 @@ mediana_colonne <- apply(load_Text_Sepsis_SIRS_EDITED, 2, median)
 # Calcola il valore minimo e massimo per tutte le colonne numeriche
 minimo_colonne <- apply(load_Text_Sepsis_SIRS_EDITED, 2, min)
 massimo_colonne <- apply(load_Text_Sepsis_SIRS_EDITED, 2, max)
+
+#ggplot(cluster1, aes(cluster1[, 1], cluster1[, 2], color = 1)) +
+#  geom_point(pch = 19) +
+#  #scale_colour_discrete(name = "Gruppi") +
+#  ggtitle("cluster1") +
+#  labs(x = "UMAP1", y = "UMAP2")+
+#  theme(legend.position = "none") 
+
+
