29e755c19b114644b94a5f2974f44a24a70a9024
Giulio Riggio <giulio.riggio.73@gmail.com>
2024-06-06 23:57:50 +0200

    Distinzione tra reference positivo o negativo

--- src/2024-05-30 tabelle colorate.R
+++ src/2024-05-30 tabelle colorate.R
@@ -20,13 +20,23 @@ rowCallback <- JS(
   "$('td:eq(0)', row).css('font-weight', 'bold');",  # Apply bold to inputval
   "$('td', row).css('font-size', '12px');",  # Apply font size reduction to all cells
   "function applyColor(value, reference, index) {",
-  "if (reference !== 0) {",
+  
+  "if (reference > 0) {",
     "if (parseFloat(value) < parseFloat(reference) - (parseFloat(reference) / 3)) {",
     "$('td:eq(' + index + ')', row).css('background-color', 'red');",
     "} else if (parseFloat(value) > parseFloat(reference) + (parseFloat(reference) / 3)) {",
     "$('td:eq(' + index + ')', row).css('background-color', 'yellow');",
     "}",
   "}",  
+  
+  "if (reference < 0) {",
+  "if (parseFloat(value) < parseFloat(reference) + (parseFloat(reference) / 3)) {",
+  "$('td:eq(' + index + ')', row).css('background-color', 'red');",
+  "} else if (parseFloat(value) > parseFloat(reference) - (parseFloat(reference) / 3)) {",
+  "$('td:eq(' + index + ')', row).css('background-color', 'yellow');",
+  "}",
+  "}",  
+  
   "}",
   "applyColor(data[2], data[1], 2);",  # Apply to column 2
   "applyColor(data[3], data[1], 3);",  # Apply to column 3
